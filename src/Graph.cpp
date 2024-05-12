2e79e9ec8e3ccf7ff528c765bb29c9fd58df432b
Luís Cordeiro <kammeratlego14@gmail.com>
2024-05-13 00:16:54 +0100

    Corrigido o erro

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -3,7 +3,10 @@
 
 // Method to add a node to the graph
 void Graph::addNode(Node *node) {
-    nodes.push_back(node);
+    if(findNode(node->getId()) == nullptr){
+        nodes.push_back(node);
+    }
+
 }
 
 // Getter method to retrieve all nodes in the graph

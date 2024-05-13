220eaf23d9c496537d8013d0814d153bcb40f21d
Luís Cordeiro <kammeratlego14@gmail.com>
2024-05-13 12:03:03 +0100

    Adicionei library para ajudar a medir tempo

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include "Menu.h"
+#include <chrono>
 
 int main() {
     Menu menu;
 
     System system;
-
+    auto timestartshere = chrono::high_resolution_clock::now();
     menu.start(system);
+    auto timefinishhere = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = timefinishhere - timestartshere;
+    cout << "Elapsed Time: " << elapsed.count() << "\n";
     return 0;
 }

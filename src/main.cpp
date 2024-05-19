9e2235a32c6aae3cdbba740f7e9b4bc8bd9ec124
Luís Cordeiro <kammeratlego14@gmail.com>
2024-05-19 19:30:14 +0100

    Limpeza da main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 #include "Menu.h"
-#include <chrono>
+
 
 int main() {
     Menu menu;
 
 
     System system;
-    auto timestartshere = chrono::high_resolution_clock::now();
     menu.start(system);
-    auto timefinishhere = chrono::high_resolution_clock::now();
-    chrono::duration<double> elapsed = timefinishhere - timestartshere;
-    cout << "Elapsed Time: " << elapsed.count() << "\n";
+
     return 0;
 }
